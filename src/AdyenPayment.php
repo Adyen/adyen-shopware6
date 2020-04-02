d29a77e341b4d1a4ada398d7156654315b943fbf
acampos1916 <angel.campos@adyen.com>
2020-04-02 15:08:57 +0200

    [PW-2246]: Initial commit and basic configuration panel

---
+++ src/AdyenPayment.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <shopware@adyen.com>
+ */
+
+namespace Adyen\Payment;
+
+use Adyen\Payment\Handlers\DummyPaymentMethodHandler;
+use Adyen\Payment\Helper\PaymentMethodsHelper;
+use Adyen\Payment\PaymentMethods\PaymentMethodInterface;
+use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Plugin\Context\ActivateContext;
+use Shopware\Core\Framework\Plugin\Context\DeactivateContext;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Shopware\Core\Framework\Plugin\Util\PluginIdProvider;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+
+
+class AdyenPayment extends Plugin
+{
+
+    public function install(InstallContext $installContext): void
+    {
+        foreach (PaymentMethodsHelper::PAYMENT_METHODS as $paymentMethod) {
+            $this->addPaymentMethod(new $paymentMethod(), $installContext->getContext());
+        }
+    }
+
+    public function activate(ActivateContext $activateContext): void
+    {
+        $this->setPaymentMethodIsActive(true, $activateContext->getContext());
+        parent::activate($activateContext);
+    }
+
+    public function deactivate(DeactivateContext $deactivateContext): void
+    {
+        $this->setPaymentMethodIsActive(false, $deactivateContext->getContext());
+        parent::deactivate($deactivateContext);
+    }
+
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        $this->setPaymentMethodIsActive(false, $uninstallContext->getContext());
+    }
+
+    private function addPaymentMethod(PaymentMethodInterface $paymentMethod, Context $context): void
+    {
+        $paymentMethodExists = $this->getPaymentMethodId();
+
+        // Payment method exists already, no need to continue here
+        if ($paymentMethodExists) {
+            return;
+        }
+
+        /** @var PluginIdProvider $pluginIdProvider */
+        $pluginIdProvider = $this->container->get(PluginIdProvider::class);
+        $pluginId = $pluginIdProvider->getPluginIdByBaseClass(get_class($this), $context);
+
+        $paymentData = [
+            'handlerIdentifier' => $paymentMethod->getPaymentHandler(),
+            'name' => $paymentMethod->getName(),
+            'description' => $paymentMethod->getDescription(),
+            'pluginId' => $pluginId,
+        ];
+
+        /** @var EntityRepositoryInterface $paymentRepository */
+        $paymentRepository = $this->container->get('payment_method.repository');
+        $paymentRepository->create([$paymentData], $context);
+    }
+
+    private function getPaymentMethodId(): ?string
+    {
+        /** @var EntityRepositoryInterface $paymentRepository */
+        $paymentRepository = $this->container->get('payment_method.repository');
+
+        // Fetch ID for update
+        $paymentCriteria = (new Criteria())->addFilter(new EqualsFilter('handlerIdentifier',
+            DummyPaymentMethodHandler::class));
+        $paymentIds = $paymentRepository->searchIds($paymentCriteria, Context::createDefaultContext());
+
+        if ($paymentIds->getTotal() === 0) {
+            return null;
+        }
+
+        return $paymentIds->getIds()[0];
+    }
+
+    private function setPaymentMethodIsActive(bool $active, Context $context): void
+    {
+        /** @var EntityRepositoryInterface $paymentRepository */
+        $paymentRepository = $this->container->get('payment_method.repository');
+
+        $paymentMethodId = $this->getPaymentMethodId();
+
+        // Payment does not even exist, so nothing to (de-)activate here
+        if (!$paymentMethodId) {
+            return;
+        }
+
+        $paymentMethod = [
+            'id' => $paymentMethodId,
+            'active' => $active,
+        ];
+
+        $paymentRepository->update([$paymentMethod], $context);
+    }
+
+}
