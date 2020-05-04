e14eaba384751f4fd966cc0b8c98364e2858cb52
attilak <attila.kiss@adyen.com>
2020-05-04 14:58:54 +0200

    [WIP] Add NotificationReceiverService.php

---
+++ src/Service/NotificationReceiverService.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <shopware@adyen.com>
+ */
+
+namespace Adyen\Shopware\Service;
+
+use Adyen\AdyenException;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+
+class NotificationReceiverService
+{
+    public function process($salesChannelContext, $request)
+    {
+        return new JsonResponse([]);
+    }
+}
