dc95549220372e1e5ebfdf5d20bdc350355636f7
acampos1916 <angel.campos@adyen.com>
2020-05-19 14:40:58 +0200

    [PW-2248]: Tab identation

--- src/Notification/NotificationEntityCollection.php
+++ src/Notification/NotificationEntityCollection.php
@@ -27,8 +27,8 @@ namespace Adyen\Shopware\Notification;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
 
 /**
- * @method void              add(NotificationEntity $entity)
- * @method void              set(string $key, NotificationEntity $entity)
+ * @method void                    add(NotificationEntity $entity)
+ * @method void                    set(string $key, NotificationEntity $entity)
  * @method NotificationEntity[]    getIterator()
  * @method NotificationEntity[]    getElements()
  * @method NotificationEntity|null get(string $key)
