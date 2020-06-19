7accf00c2839b2c03236e6aae428e7d5702724a2
acampos1916 <angel.campos@adyen.com>
2020-06-19 15:09:44 +0200

    [PW-2251]: Removing unsued method in CurrencyService.php

--- src/Service/CurrencyService.php
+++ src/Service/CurrencyService.php
@@ -24,38 +24,8 @@
 namespace Adyen\Shopware\Service;
 
 use Adyen\Util\Currency;
-use Shopware\Core\Checkout\Order\OrderEntity;
-use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\System\Currency\CurrencyEntity;
-use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
-use RuntimeException;
 
 class CurrencyService extends Currency
 {
-
-    /**
-     * @var EntityRepositoryInterface $currencyRepository
-     */
-    private $currencyRepository;
-
-    public function __construct(EntityRepositoryInterface $currencyRepository)
-    {
-        $this->currencyRepository = $currencyRepository;
-    }
-
-    public function getOrderCurrency(OrderEntity $order, Context $context): CurrencyEntity
-    {
-        $criteria = new Criteria([$order->getCurrencyId()]);
-
-        /** @var null|CurrencyEntity $currency */
-        $currency = $this->currencyRepository->search($criteria, $context)->first();
-
-        if (null === $currency) {
-            throw new RuntimeException('missing order currency entity');
-        }
-
-        return $currency;
-    }
 
 }
