58847eee8016105554ebb5aa408e275731708b9e
marcoss <marcos.silvagarcia@adyen.com>
2020-07-23 11:24:17 +0200

    Create PaymentResponseEntity and its' correlates

---
+++ src/Entity/PaymentResponse/PaymentResponseEntity.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen plugin for Shopware 6
+ *
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ */
+
+namespace Adyen\Shopware\Entity\PaymentResponse;
+
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityIdTrait;
+
+class PaymentResponseEntity extends Entity
+{
+    use EntityIdTrait;
+
+    /**
+     * @var string
+     */
+    protected $token;
+
+    /**
+     * @var string
+     */
+    protected $resultCode;
+
+    /**
+     * @var string
+     */
+    protected $response;
+
+    /**
+     * @return string
+     */
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     */
+    public function setToken(string $token): void
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return string
+     */
+    public function getResultCode(): string
+    {
+        return $this->resultCode;
+    }
+
+    /**
+     * @param string $resultCode
+     */
+    public function setResultCode(string $resultCode): void
+    {
+        $this->resultCode = $resultCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getResponse(): string
+    {
+        return $this->response;
+    }
+
+    /**
+     * @param string $response
+     */
+    public function setResponse(string $response): void
+    {
+        $this->response = $response;
+    }
+}
---
+++ src/Service/PaymentResponseService.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen plugin for Shopware 6
+ *
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ */
+
+namespace Adyen\Shopware\Service;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+
+class PaymentResponseService
+{
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $repository;
+
+    public function __construct(
+        EntityRepositoryInterface $repository
+    ) {
+        $this->repository = $repository;
+    }
+
+    public function getPaymentResponseForMerchantReference(string $merchantReference)
+    {
+        return $this->repository
+            ->search(
+                (new Criteria())
+                    ->addFilter(new EqualsFilter('order_number', $merchantReference))
+                    ->addAssociation('cart'),
+                Context::createDefaultContext()
+            )
+            ->first();
+    }
+}
