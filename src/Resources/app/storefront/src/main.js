fcc2427e8134a3cadc34df4ade984c28262e510c
acampos1916 <angel.campos@adyen.com>
2020-07-28 17:33:37 +0200

    [PW-2741] New confirm-order JS plugin

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,6 +1,8 @@
 // Import all necessary Storefront plugins and scss files
 import CheckoutPlugin from './checkout/checkout.plugin';
+import ConfirmOrderPlugin from './checkout/confirm-order.plugin';
 
 // Register them via the existing PluginManager
 const PluginManager = window.PluginManager;
 PluginManager.register('CheckoutPlugin', CheckoutPlugin, '[data-adyen-payment]');
+PluginManager.register('ConfirmOrderPlugin', ConfirmOrderPlugin, '[data-adyen-payment]');
