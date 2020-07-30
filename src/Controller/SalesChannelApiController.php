c8da14257612b608a25ac79bce4b6f9516b2e3d8
acampos1916 <angel.campos@adyen.com>
2020-07-30 16:58:40 +0200

    [PW-2741] Removing duplicated use statement

--- src/Controller/SalesChannelApiController.php
+++ src/Controller/SalesChannelApiController.php
@@ -32,7 +32,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Adyen\Shopware\Service\OriginKeyService;
