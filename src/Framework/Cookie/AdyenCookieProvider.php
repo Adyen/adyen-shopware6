03c09051c62af83f7db2cfb3d0a6f79ca5db9a9a
attilak <attila.kiss@adyen.com>
2020-08-06 17:28:04 +0200

    Phpcs fix and formatting

--- src/Framework/Cookie/AdyenCookieProvider.php
+++ src/Framework/Cookie/AdyenCookieProvider.php
@@ -18,63 +18,63 @@ class AdyenCookieProvider implements CookieProviderInterface
      *
      * @param CookieProviderInterface $service
      */
-    function __construct(CookieProviderInterface $service)
+    public function __construct(CookieProviderInterface $service)
     {
         $this->originalService = $service;
     }
 
     // cookies can also be provided as a group
     private static $requiredCookies = [
-            [
-                'cookie' => 'JSESSIONID',
-                'snippet_name' => 'adyen.required_cookie.name',
-                'snippet_description' => 'adyen.required_cookie.description'
-            ],
-            [
-                'cookie' => '_uetvid',
-                "hidden" => true
-            ],
-            [
-                'cookie' => '_uetsid',
-                "hidden" => true
-            ],
-            [
-                'cookie' => '__cfduid',
-                "hidden" => true
-            ],
-            [
-                'cookie' => '_mkto_trk',
-                "hidden" => true
-            ],
-            [
-                'cookie' => '_hjid',
-                "hidden" => true
-            ],
-            [
-                'cookie' => 'lastUpdatedGdpr',
-                "hidden" => true
-            ],
-            [
-                'cookie' => 'gdpr',
-                "hidden" => true
-            ],
-            [
-                'cookie' => '_fbp',
-                "hidden" => true
-            ],
-            [
-                'cookie' => '_ga',
-                "hidden" => true
-            ],
-            [
-                'cookie' => '_gid',
-                "hidden" => true
-            ],
-            [
-                'cookie' => '_gcl_au',
-                "hidden" => true
-            ]
-            /* TODO check if it can be retrieved automatically or only manually */
+        [
+            'cookie' => 'JSESSIONID',
+            'snippet_name' => 'adyen.required_cookie.name',
+            'snippet_description' => 'adyen.required_cookie.description'
+        ],
+        [
+            'cookie' => '_uetvid',
+            "hidden" => true
+        ],
+        [
+            'cookie' => '_uetsid',
+            "hidden" => true
+        ],
+        [
+            'cookie' => '__cfduid',
+            "hidden" => true
+        ],
+        [
+            'cookie' => '_mkto_trk',
+            "hidden" => true
+        ],
+        [
+            'cookie' => '_hjid',
+            "hidden" => true
+        ],
+        [
+            'cookie' => 'lastUpdatedGdpr',
+            "hidden" => true
+        ],
+        [
+            'cookie' => 'gdpr',
+            "hidden" => true
+        ],
+        [
+            'cookie' => '_fbp',
+            "hidden" => true
+        ],
+        [
+            'cookie' => '_ga',
+            "hidden" => true
+        ],
+        [
+            'cookie' => '_gid',
+            "hidden" => true
+        ],
+        [
+            'cookie' => '_gcl_au',
+            "hidden" => true
+        ]
+        /* TODO check if it can be retrieved automatically or only manually */
     ];
 
     /**
