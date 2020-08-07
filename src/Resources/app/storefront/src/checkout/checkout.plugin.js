3dadf2e7cef508d06363f1b2ee7be4c649579afc
marcoss <marcos.silvagarcia@adyen.com>
2020-08-07 18:44:02 +0200

    Validate card form before saving info

--- src/Resources/app/storefront/src/checkout/checkout.plugin.js
+++ src/Resources/app/storefront/src/checkout/checkout.plugin.js
@@ -1,20 +1,48 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen plugin for Shopware 6
+ *
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ */
+
+import DomAccess from 'src/helper/dom-access.helper';
 import Plugin from 'src/plugin-system/plugin.class';
 import StoreApiClient from 'src/service/store-api-client.service';
+import validatePaymentMethod from '../validator/paymentMethod';
+import CardFormValidator from '../validator/CardFormValidator';
+
 
 /* global adyenCheckoutConfiguration, AdyenCheckout, adyenCheckoutOptions */
 /* eslint-disable no-unused-vars */
 export default class CheckoutPlugin extends Plugin {
 
     init() {
+        const confirmPaymentForm = DomAccess.querySelector(document, '#confirmPaymentForm');
+        confirmPaymentForm.addEventListener('submit', this.onConfirmPayment.bind(this));
 
         const formattedHandlerIdentifier = 'handler_adyen_cardspaymentmethodhandler';
 
-        const paymentMethodTypeHandlers = {
+        this.paymentMethodTypeHandlers = {
             'scheme': formattedHandlerIdentifier
         };
 
         //PMs that should show an 'Update Details' button if there's already a state.data for that PM stored for this context
-        const updatablePaymentMethods = ['scheme'];
+        this.updatablePaymentMethods = ['scheme'];
 
         this.client = new StoreApiClient();
 
@@ -53,77 +81,14 @@ export default class CheckoutPlugin extends Plugin {
 
         window.adyenCheckout = new AdyenCheckout(ADYEN_CHECKOUT_CONFIG);
 
-        var placeOrderAllowed = false;
-        var data;
+        this.placeOrderAllowed = false;
+        this.data = '';
 
         // use this object to iterate through the stored payment methods
         var paymentMethods = window.adyenCheckout.paymentMethodsResponse.paymentMethods;
 
         // Iterate through the payment methods list we got from the adyen checkout component
-        paymentMethods.forEach(function (paymentMethod) {
-            //  if the container doesn't exits don't try to render the component
-            var paymentMethodContainer = $('[data-adyen-payment-method="' + paymentMethodTypeHandlers[paymentMethod.type] + '"]');
-
-            // container doesn't exist, something went wrong on the template side
-            if (!paymentMethodContainer.length) {
-                return;
-            }
-
-            // If payment method doesn't have details, just skip it
-            if (!paymentMethod.details) {
-                return;
-            }
-
-            //Show the payment method's contents if it's selected by default
-            if ($('[data-adyen-payment-method-id]').data('adyen-payment-method-id') == $('[name=paymentMethodId]:checked').val()) {
-                $('[data-adyen-payment-method-id]').show();
-            }
-
-            //Hide other payment method's contents when selecting an option
-            $('[name=paymentMethodId]').on("change", function () {
-                $('.adyen-payment-method-container-div').hide();
-                $('[data-adyen-payment-method-id="' + $(this).val() + '"]').show();
-            });
-
-            /*Use the storedPaymentMethod object and the custom onChange function as the configuration object together*/
-            var configuration = Object.assign(paymentMethod, {
-                'onChange': function (state) {
-                    if (state.isValid) {
-                        data = state.data;
-                        $('#adyenStateData').val(JSON.stringify(data));
-                        $('#adyenOrigin').val(window.location.origin);
-                        placeOrderAllowed = true;
-                    } else {
-                        placeOrderAllowed = false;
-                        resetFields();
-                    }
-                }
-            });
-
-            try {
-
-                window.adyenCheckout
-                    .create(paymentMethod.type, configuration)
-                    .mount(paymentMethodContainer.find('[data-adyen-payment-container]').get(0));
-            } catch (err) {
-                console.log(err);
-            }
-
-            //Hiding component contents if there's already state.data saved for this PM
-            if (updatablePaymentMethods.includes(paymentMethod.type) && adyenCheckoutOptions.statedataPaymentMethod === paymentMethod.type) {
-                $('[data-adyen-payment-container]').hide();
-                $('[data-adyen-update-payment-details]').show();
-            }
-
-
-        });
-
-        /**
-         * Reset card details
-         */
-        function resetFields() {
-            data = "";
-        }
+        paymentMethods.forEach(this.renderPaymentMethod.bind(this));
 
         /**
          * Shows the payment method component in order to update the previously saved details
@@ -134,5 +99,82 @@ export default class CheckoutPlugin extends Plugin {
         }
 
         /* eslint-enable no-unused-vars */
+    }
+
+    renderPaymentMethod (paymentMethod) {
+        //  if the container doesn't exits don't try to render the component
+        var paymentMethodContainer = $('[data-adyen-payment-method="' + this.paymentMethodTypeHandlers[paymentMethod.type] + '"]');
+
+        // container doesn't exist, something went wrong on the template side
+        if (!paymentMethodContainer.length) {
+            return;
+        }
+
+        // If payment method doesn't have details, just skip it
+        if (!paymentMethod.details) {
+            return;
+        }
+
+        //Show the payment method's contents if it's selected by default
+        if (validatePaymentMethod()) {
+            $('[data-adyen-payment-method-id]').show();
+        }
+
+        //Hide other payment method's contents when selecting an option
+        $('[name=paymentMethodId]').on("change", function () {
+            $('.adyen-payment-method-container-div').hide();
+            $('[data-adyen-payment-method-id="' + $(this).val() + '"]').show();
+        });
+
+        /*Use the storedPaymentMethod object and the custom onChange function as the configuration object together*/
+        var configuration = Object.assign(paymentMethod, {
+            'onChange': this.onPaymentMethodChange.bind(this)
+        });
+
+        try {
+            const paymentMethodInstance = window.adyenCheckout
+                .create(paymentMethod.type, configuration);
+
+            if (paymentMethod.type === 'scheme') {
+                this.cardFormValidator = new CardFormValidator(paymentMethodInstance);
+            }
+
+            paymentMethodInstance.mount(paymentMethodContainer.find('[data-adyen-payment-container]').get(0));
+        } catch (err) {
+            console.log(err);
+        }
+
+        //Hiding component contents if there's already state.data saved for this PM
+        if (this.updatablePaymentMethods.includes(paymentMethod.type) && adyenCheckoutOptions.statedataPaymentMethod === paymentMethod.type) {
+            $('[data-adyen-payment-container]').hide();
+            $('[data-adyen-update-payment-details]').show();
+        }
+
+
+    }
+
+    /**
+     * Reset card details
+     */
+    resetFields () {
+        this.data = '';
+    }
+
+    onConfirmPayment (event) {
+        if (!this.cardFormValidator.validateForm()) {
+            event.preventDefault();
+        }
+    }
+
+    onPaymentMethodChange (state) {
+        if (state.isValid) {
+            this.data = state.data;
+            $('#adyenStateData').val(JSON.stringify(this.data));
+            $('#adyenOrigin').val(window.location.origin);
+            this.placeOrderAllowed = true;
+        } else {
+            this.placeOrderAllowed = false;
+            this.resetFields();
+        }
     }
 }
