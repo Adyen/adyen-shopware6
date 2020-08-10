f28e88738bec133ed41c22e6c11fe45f070e7522
Marcos Garcia <marcos.asgarcia@gmail.com>
2020-08-10 13:07:50 +0200

    Validate card form before saving info (#40)
    Co-authored-by: marcoss <marcos.silvagarcia@adyen.com>

---
+++ src/Resources/app/storefront/src/validator/CardFormValidator.js
@@ -0,0 +1,32 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen plugin for Shopware 6
+ *
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ */
+
+export default class CardFormValidator {
+    constructor(cardInstance) {
+        this.cardInstance = cardInstance;
+    }
+
+    validateForm() {
+        this.cardInstance.showValidation();
+        return this.cardInstance.isValid;
+    }
+}
