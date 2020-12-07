ab52e84d5a17b468b315c52f21b08c8b6e32165a
petero <peter.ojo@adyen.com>
2020-12-07 18:27:54 +0100

    [PW-3587] create and register scheduled tasks for notifications

---
+++ src/ScheduledTask/ProcessNotificationsHandler.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <shopware@adyen.com>
+ */
+
+namespace Adyen\Shopware\ScheduledTask;
+
+use Shopware\Core\Framework\MessageQueue\ScheduledTask\ScheduledTaskHandler;
+
+class ProcessNotificationsHandler extends ScheduledTaskHandler
+{
+    public static function getHandledMessages(): iterable
+    {
+        return [ ProcessNotifications::class ];
+    }
+
+    public function run()
+    {
+        // import logger and add debug lines
+    }
+}
