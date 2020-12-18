935dc8b89c1ee9fcb0b611b14e6b575a68b453e0
Ángel Campos <angel.campos@adyen.com>
2020-12-18 10:55:07 +0100

    [PW-3631] storedPaymentMethods state.data check (#99)
    * [PW-3631] Correcting Klarna formatted handler identifiers
    
    * [PW-3631] New dist JS file
    
    * [PW-3631] New delete state data from context token function
    
    * [PW-3631] Checking if state data and PM selected match before saving
    
    * [PW-3631] phpcs fix
    
    * [PW-3631] phpcs fix
    
    * [PW-3631] phpcs fix
    
    * [PW-3631] Handling state.data JSON errors
    
    * [PW-3631] Handling state.data save errors

--- src/Subscriber/PaymentSubscriber.php
+++ src/Subscriber/PaymentSubscriber.php
@@ -29,7 +29,15 @@ use Adyen\Shopware\Handlers\OneClickPaymentMethodHandler;
 use Adyen\Shopware\Service\ConfigurationService;
 use Adyen\Shopware\Service\OriginKeyService;
 use Adyen\Shopware\Service\PaymentMethodsService;
+use Adyen\Shopware\Service\PaymentStateDataService;
 use Adyen\Shopware\Service\Repository\SalesChannelRepository;
+use JsonException;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Plugin\Util\PluginIdProvider;
 use Shopware\Core\Framework\Struct\ArrayEntity;
 use Shopware\Core\System\SalesChannel\Event\SalesChannelContextSwitchEvent;
@@ -39,7 +47,7 @@ use Shopware\Storefront\Page\Account\Order\AccountEditOrderPageLoader;
 use Shopware\Storefront\Page\Checkout\Confirm\CheckoutConfirmPageLoadedEvent;
 use Shopware\Storefront\Page\PageLoadedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Adyen\Shopware\Service\PaymentStateDataService;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\RouterInterface;
 
 class PaymentSubscriber implements EventSubscriberInterface
@@ -83,6 +91,26 @@ class PaymentSubscriber implements EventSubscriberInterface
     private $pluginIdProvider;
 
     /**
+     * @var EntityRepositoryInterface $paymentMethodRepository
+     */
+    private $paymentMethodRepository;
+
+    /**
+     * @var SessionInterface $session
+     */
+    private $session;
+
+    /**
+     * @var ContainerInterface $container
+     */
+    private $container;
+
+    /**
+     * @var LoggerInterface $logger
+     */
+    private $logger;
+
+    /**
      * PaymentSubscriber constructor.
      *
      * @param PaymentStateDataService $paymentStateDataService
@@ -92,6 +120,8 @@ class PaymentSubscriber implements EventSubscriberInterface
      * @param ConfigurationService $configurationService
      * @param PaymentMethodsService $paymentMethodsService
      * @param PluginIdProvider $pluginIdProvider
+     * @param EntityRepositoryInterface $paymentMethodRepository
+     * @param LoggerInterface $logger
      */
     public function __construct(
         PaymentStateDataService $paymentStateDataService,
@@ -100,7 +130,11 @@ class PaymentSubscriber implements EventSubscriberInterface
         SalesChannelRepository $salesChannelRepository,
         ConfigurationService $configurationService,
         PaymentMethodsService $paymentMethodsService,
-        PluginIdProvider $pluginIdProvider
+        PluginIdProvider $pluginIdProvider,
+        EntityRepositoryInterface $paymentMethodRepository,
+        SessionInterface $session,
+        ContainerInterface $container,
+        LoggerInterface $logger
     ) {
         $this->paymentStateDataService = $paymentStateDataService;
         $this->router = $router;
@@ -109,6 +143,11 @@ class PaymentSubscriber implements EventSubscriberInterface
         $this->configurationService = $configurationService;
         $this->paymentMethodsService = $paymentMethodsService;
         $this->pluginIdProvider = $pluginIdProvider;
+        $this->paymentMethodRepository = $paymentMethodRepository;
+        $this->paymentMethodRepository = $paymentMethodRepository;
+        $this->session = $session;
+        $this->container = $container;
+        $this->logger = $logger;
     }
 
     /**
@@ -130,16 +169,8 @@ class PaymentSubscriber implements EventSubscriberInterface
      */
     public function onContextTokenUpdate(SalesChannelContextSwitchEvent $event)
     {
-        if ($event->getRequestDataBag()->get('adyenStateData')) {
-            try {
-                $this->paymentStateDataService->insertPaymentStateData(
-                    $event->getSalesChannelContext()->getToken(),
-                    $event->getRequestDataBag()->get('adyenStateData'),
-                    $event->getRequestDataBag()->get('adyenOrigin')
-                );
-            } catch (AdyenException $exception) {
-                //TODO handle error
-            }
+        if (!$this->saveStateData($event)) {
+            $this->session->getFlashBag()->add('danger', $this->trans('adyen.paymentMethodSelectionError'));
         }
     }
 
@@ -276,5 +307,61 @@ class PaymentSubscriber implements EventSubscriberInterface
             }
         }
         return $originalPaymentMethods;
+    }
+
+    private function trans(string $snippet, array $parameters = []): string
+    {
+        return $this->container
+            ->get('translator')
+            ->trans($snippet, $parameters);
+    }
+
+    private function saveStateData(SalesChannelContextSwitchEvent $event): bool
+    {
+        //State data from the frontend
+        $stateData = $event->getRequestDataBag()->get('adyenStateData');
+
+        if ($stateData) {
+            //Convert the state data into an array
+            $stateDataArray = json_decode($stateData, true);
+
+            if (json_last_error() !== JSON_ERROR_NONE) {
+                $this->logger->error('Payment state data is an invalid JSON: ' . json_last_error_msg());
+                return false;
+            }
+
+            //Payment method selected from the Shopware methods form
+            $selectedPaymentMethod = $this->paymentMethodRepository->search(
+                (new Criteria())
+                    ->addFilter(new EqualsFilter('id', $event->getRequestDataBag()->get('paymentMethodId'))),
+                Context::createDefaultContext()
+            )->first();
+
+            $selectedPaymentMethodIsStoredPM =
+                $selectedPaymentMethod->getFormattedHandlerIdentifier() == 'handler_adyen_oneclickpaymentmethodhandler';
+
+            $stateDataIsStoredPM = !empty($stateDataArray["paymentMethod"]["storedPaymentMethodId"]);
+
+            //Only store the state data if it matches the selected PM
+            if ($stateDataIsStoredPM == $selectedPaymentMethodIsStoredPM) {
+                try {
+                    $this->paymentStateDataService->insertPaymentStateData(
+                        $event->getSalesChannelContext()->getToken(),
+                        $event->getRequestDataBag()->get('adyenStateData'),
+                        $event->getRequestDataBag()->get('adyenOrigin')
+                    );
+                } catch (AdyenException $exception) {
+                    return false;
+                }
+            } else {
+                //PM selected and state.data don't match, clear previous state.data
+                $this->paymentStateDataService->deletePaymentStateDataFromContextToken(
+                    $event->getSalesChannelContext()->getToken()
+                );
+            }
+            return true;
+        }
+        $this->logger->error('Payment state data not available.');
+        return false;
     }
 }
