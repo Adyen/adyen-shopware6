405b20e3d340ea83a8849145b1bc773d81eed6c3
Andreas Allacher <andreas.allacher@massiveart.com>
2021-04-20 11:23:46 +0200

    make adyen payment responess unavailable in store api

--- src/Core/Checkout/Order/Aggregate/OrderTransaction/OrderTransactionExtension.php
+++ src/Core/Checkout/Order/Aggregate/OrderTransaction/OrderTransactionExtension.php
@@ -25,7 +25,10 @@ namespace Adyen\Shopware\Core\Checkout\Order\Aggregate\OrderTransaction;
 
 use Adyen\Shopware\Entity\PaymentResponse\PaymentResponseEntityDefinition;
 use Shopware\Core\Checkout\Order\Aggregate\OrderTransaction\OrderTransactionDefinition;
+use Shopware\Core\Framework\Api\Context\SalesChannelApiSource;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityExtension;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\ApiAware;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\ReadProtected;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\OneToManyAssociationField;
 use Shopware\Core\Framework\DataAbstractionLayer\FieldCollection;
 
@@ -41,12 +44,17 @@ class OrderTransactionExtension extends EntityExtension
 
     public function extendFields(FieldCollection $collection): void
     {
-        $collection->add(
-            new OneToManyAssociationField(
-                'adyenPaymentResponses',
-                PaymentResponseEntityDefinition::class,
-                'order_transaction_id'
-            )
+        $field = new OneToManyAssociationField(
+            'adyenPaymentResponses',
+            PaymentResponseEntityDefinition::class,
+            'order_transaction_id'
         );
+
+        // Ensure the data is not available via the Store API in older Shopware versions.
+        if (!class_exists(ApiAware::class) && class_exists(ReadProtected::class)) {
+            $field->addFlags(new ReadProtected(SalesChannelApiSource::class));
+        }
+
+        $collection->add($field);
     }
 }
