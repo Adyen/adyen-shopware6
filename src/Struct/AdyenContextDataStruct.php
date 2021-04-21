61d11ac37d45ed0a3209a7e12ab292202d7fa447
Andreas Allacher <AndreasA@users.noreply.github.com>
2021-04-21 10:03:38 +0200

    Add client key, environment and has payment state data to context store api response (#163)
    * Add client key, environment and has payment state data to context store api response
    
    * Update src/Subscriber/ContextResolvedSubscriber.php
    
    Co-authored-by: Peter Ojo <peterojo@users.noreply.github.com>
    
    * Update src/Struct/ContextAdyenDataStruct.php
    
    Co-authored-by: Peter Ojo <peterojo@users.noreply.github.com>
    
    * rename class
    
    Co-authored-by: Andreas Allacher <andreas.allacher@massiveart.com>
    Co-authored-by: Peter Ojo <peterojo@users.noreply.github.com>

---
+++ src/Struct/AdyenContextDataStruct.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2021 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <shopware@adyen.com>
+ */
+
+namespace Adyen\Shopware\Struct;
+
+use Shopware\Core\Framework\Struct\Struct;
+
+class AdyenContextDataStruct extends Struct
+{
+    /**
+     * @var string|null
+     */
+    protected $clientKey = null;
+
+    /**
+     * @var string|null
+     */
+    protected $environment = null;
+
+    /**
+     * @var bool
+     */
+    protected $hasPaymentStateData = false;
+
+    public function getClientKey(): ?string
+    {
+        return $this->clientKey;
+    }
+
+    public function setClientKey(?string $clientKey): void
+    {
+        $this->clientKey = $clientKey;
+    }
+
+    public function getEnvironment(): ?string
+    {
+        return $this->environment;
+    }
+
+    public function setEnvironment(?string $environment): void
+    {
+        $this->environment = $environment;
+    }
+
+    public function isHasPaymentStateData(): bool
+    {
+        return $this->hasPaymentStateData;
+    }
+
+    public function setHasPaymentStateData(bool $hasPaymentStateData): void
+    {
+        $this->hasPaymentStateData = $hasPaymentStateData;
+    }
+}
