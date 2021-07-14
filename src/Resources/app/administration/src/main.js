379fbbd0b49bf55c64f8f974b298f3d4fe9ab8db
jeant <jean.teuma@adyen.com>
2021-07-14 12:04:14 +0200

    PW-4866 - Create new twig and js file to extend functionality in the order details page

--- src/Resources/app/administration/src/main.js
+++ src/Resources/app/administration/src/main.js
@@ -14,7 +14,7 @@
  *
  * Adyen plugin for Shopware 6
  *
- * Copyright (c) 2020 Adyen B.V.
+ * Copyright (c) 2021 Adyen B.V.
  * This file is open source and available under the MIT license.
  * See the LICENSE file for more info.
  *
@@ -22,6 +22,7 @@
 
 import './service/adyenConfigCheckService';
 import './component/adyen-config-check-button';
+import './sw-order-detail-base-override/index.js'
 
 import localeEnGb from './snippet/en_GB.json';
 
---
+++ src/Resources/app/administration/src/sw-order-detail-base-override/index.js
@@ -0,0 +1,5 @@
+import template from './sw-order-detail-base.html.twig';
+
+Shopware.Component.override('sw-order-detail-base', {
+    template
+});
