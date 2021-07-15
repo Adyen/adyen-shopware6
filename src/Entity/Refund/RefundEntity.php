74e35df8bd962e2912ccbc07112ef4e9e5456353
jeant <jean.teuma@adyen.com>
2021-07-15 11:25:02 +0200

    PW-4866 - Create new refund entities and migration script

---
+++ src/Entity/Refund/RefundEntity.php
@@ -0,0 +1,187 @@
+<?php declare(strict_types=1);
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen plugin for Shopware 6
+ *
+ * Copyright (c) 2021 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ */
+
+namespace Adyen\Shopware\Entity\Refund;
+
+use Shopware\Core\Checkout\Order\OrderEntity;
+use Shopware\Core\Framework\DataAbstractionLayer\Entity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityIdTrait;
+
+class RefundEntity extends Entity
+{
+    const SOURCE_ADYEN = 'Adyen Platform';
+    const SOURCE_SHOPWARE = 'Shopware';
+
+    const STATUS_SUCCESS = 'Success';
+    const STATUS_FAILED = 'Failed';
+    const STATUS_PENDING = 'Pending';
+
+    use EntityIdTrait;
+
+    /**
+     * @var string
+     */
+    protected string $orderId;
+
+    /**
+     * @var OrderEntity
+     */
+    protected OrderEntity $order;
+
+    /**
+     * @var string
+     */
+    protected string $pspReference;
+
+    /**
+     * @var string
+     */
+    protected string $source;
+
+    /**
+     * @var string
+     */
+    protected string $status;
+
+    /**
+     * @var \DateTimeInterface|null
+     */
+    protected $createdAt;
+
+    /**
+     * @var int
+     */
+    protected int $amount;
+
+    /**
+     * @return string
+     */
+    public function getOrderId(): string
+    {
+        return $this->orderId;
+    }
+
+    /**
+     * @param string $orderId
+     */
+    public function setOrderId(string $orderId): void
+    {
+        $this->orderId = $orderId;
+    }
+
+    /**
+     * @return OrderEntity
+     */
+    public function getOrder(): OrderEntity
+    {
+        return $this->order;
+    }
+
+    /**
+     * @param OrderEntity $order
+     */
+    public function setOrder(OrderEntity $order): void
+    {
+        $this->order = $order;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPspReference(): string
+    {
+        return $this->pspReference;
+    }
+
+    /**
+     * @param string $pspReference
+     */
+    public function setPspReference(string $pspReference): void
+    {
+        $this->pspReference = $pspReference;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSource(): string
+    {
+        return $this->source;
+    }
+
+    /**
+     * @param string $source
+     */
+    public function setSource(string $source): void
+    {
+        $this->source = $source;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param string $status
+     */
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTimeInterface|null $createdAt
+     */
+    public function setCreatedAt(?\DateTimeInterface $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAmount(): int
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param int $amount
+     */
+    public function setAmount(int $amount): void
+    {
+        $this->amount = $amount;
+    }
+}
---
+++ src/Entity/Refund/RefundEntityCollection.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2021 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <shopware@adyen.com>
+ */
+
+namespace Adyen\Shopware\Entity\Refund;
+
+use Shopware\Core\Framework\DataAbstractionLayer\EntityCollection;
+
+/**
+ * @method void              add(RefundEntity $entity)
+ * @method void              set(string $key, RefundEntity $entity)
+ * @method RefundEntity[]    getIterator()
+ * @method RefundEntity[]    getElements()
+ * @method RefundEntity|null get(string $key)
+ * @method RefundEntity|null first()
+ * @method RefundEntity|null last()
+ */
+class RefundEntityCollection extends EntityCollection
+{
+    protected function getExpectedClass(): string
+    {
+        return RefundEntity::class;
+    }
+}
