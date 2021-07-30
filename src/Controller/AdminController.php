7fb6534388d61394ab3525b6bd5969883356699a
jeant <jean.teuma@adyen.com>
2021-07-30 15:56:07 +0200

    PW-4866 - Update route names and translation

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -132,7 +132,7 @@ class AdminController
      *
      * @Route(
      *     "/api/adyen/refunds",
-     *     name="api.action.adyen.refund",
+     *     name="api.adyen_refund.post",
      *     methods={"POST"}
      * )
      *
@@ -200,7 +200,7 @@ class AdminController
     /**
      * @Route(
      *     "/api/adyen/orders/{orderId}/refunds",
-     *     name="api.action.adyen.refund",
+     *     name="api.adyen_refund.get",
      *     methods={"GET"}
      * )
      *
