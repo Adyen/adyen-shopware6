8295e8c24916b8c3630d9aa3b957a226107c08e8
jeant <jean.teuma@adyen.com>
2021-08-04 14:48:12 +0200

    Revert "PW-4914 - Create function in configurationService to get the new config"
    This reverts commit 0f10357d2e3c4b46c7aaef78304a692edd4d4558.

--- src/Service/ConfigurationService.php
+++ src/Service/ConfigurationService.php
@@ -195,13 +195,4 @@ class ConfigurationService
 
         return $this->getClientKeyTest($salesChannelId);
     }
-
-    /**
-     * @param string|null $salesChannelId
-     * @return bool
-     */
-    public function getEnableSaveCreditCard(string $salesChannelId = null): bool
-    {
-        return !!$this->systemConfigService->get(self::BUNDLE_NAME . '.config.enableSaveCreditCard', $salesChannelId);
-    }
 }
