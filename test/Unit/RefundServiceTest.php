1d910de81ed33f3fcabc119017ddefe3ac247ca7
jeant <jean.teuma@adyen.com>
2021-08-12 13:27:38 +0200

    PW-4912 - Add test bootstrap file, phpunit xml and an empty test class

---
+++ test/Unit/RefundServiceTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Payment Module
+ *
+ * Copyright (c) 2021 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ *
+ * Author: Adyen <shopware@adyen.com>
+ */
+
+namespace Adyen\Shopware\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class RefundServiceTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
