cf29622ce26a2611dd95f95089616cbcc2bc1ef0
peterojo <peter.ojo@adyen.com>
2022-10-18 13:16:55 +0200

    [PW-7218] mount giftcard components on click

--- src/Resources/app/storefront/src/cart/cart.plugin.js
+++ src/Resources/app/storefront/src/cart/cart.plugin.js
@@ -21,29 +21,48 @@
  */
 
 import Plugin from 'src/plugin-system/plugin.class';
+import DomAccess from 'src/helper/dom-access.helper';
 import StoreApiClient from 'src/service/store-api-client.service';
+import ElementLoadingIndicatorUtil from 'src/utility/loading-indicator/element-loading-indicator.util';
 
 export default class CartPlugin extends Plugin {
     init() {
         this._client = new StoreApiClient();
         this.adyenCheckout = Promise;
-        this.initializeCheckoutComponent().bind(this);
+        this.initializeCheckoutComponent().then(function () {
+            this.listenForGiftcardSelection();
+        }.bind(this));
     }
 
-    async initializeCheckoutComponent () {
+    async initializeCheckoutComponent() {
         const { locale, clientKey, environment } = adyenCheckoutConfiguration;
-        const giftcards = JSON.parse(adyenGiftcardsConfiguration.giftcards);
 
         const ADYEN_CHECKOUT_CONFIG = {
             locale,
             clientKey,
-            environment
+            environment,
+            showPayButton: true
         };
 
         this.adyenCheckout = await AdyenCheckout(ADYEN_CHECKOUT_CONFIG);
     }
 
-    mountGiftcardComponent (giftcardData) {
+    listenForGiftcardSelection() {
+        $('.adyen-giftcard').on('click', function (event) {
+            ElementLoadingIndicatorUtil.create(DomAccess.querySelector(document, '#adyen-giftcard-component'));
+            let payload = JSON.parse(event.currentTarget.dataset.payload);
+            this.mountGiftcardComponent(payload);
+        }.bind(this));
+    }
 
+    mountGiftcardComponent(payload) {
+        try {
+            const paymentMethodInstance = this.adyenCheckout.create('giftcard', payload);
+            paymentMethodInstance.mount('#adyen-giftcard-component');
+
+        } catch (e) {
+            console.log('giftcard not available');
+        }
+        ElementLoadingIndicatorUtil.remove(DomAccess.querySelector(document, '#adyen-giftcard-component'));
     }
 }
