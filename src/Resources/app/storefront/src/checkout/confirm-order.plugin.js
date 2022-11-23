be4cdd8fbaffbc8c897be4d54b73702424722fdf
Can Demiralp <ecandemiralp@gmail.com>
2022-11-23 10:22:54 +0100

    [PW-7429] - Support new GooglePay tx_variant (#317)

--- src/Resources/app/storefront/src/checkout/confirm-order.plugin.js
+++ src/Resources/app/storefront/src/checkout/confirm-order.plugin.js
@@ -276,6 +276,16 @@ export default class ConfirmOrderPlugin extends Plugin {
         let selectedPaymentMethod = this.adyenCheckout.paymentMethodsResponse.paymentMethods
             .filter(item => item.type === this.selectedAdyenPaymentMethod);
 
+        /*
+         * If the PM is GooglePay in Shopware, check for the `paywithgoogle` tx_variant also in paymentMethods response.
+         * This block should be remove after depreating the `paywithgoogle` tx_variant.
+         */
+        // TODO: Following block will be removed after the deprecation of the `paywithgoogle` tx_variant.
+        if (selectedPaymentMethod.length < 1 && this.selectedAdyenPaymentMethod === 'googlepay') {
+            selectedPaymentMethod = this.adyenCheckout.paymentMethodsResponse.paymentMethods
+                .filter(item => item.type === 'paywithgoogle');
+        }
+
         if (selectedPaymentMethod.length < 1) {
             return;
         }
