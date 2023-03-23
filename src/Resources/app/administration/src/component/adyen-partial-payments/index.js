dd7a736f9981582ca575c898668b64fd1db6a698
Can Demiralp <ecandemiralp@gmail.com>
2023-03-23 09:29:30 +0100

    [PW-8273] Create an override and a new Vue component for partial payments

--- src/Resources/app/administration/src/component/adyen-partial-payments/index.js
+++ src/Resources/app/administration/src/component/adyen-partial-payments/index.js
@@ -14,22 +14,29 @@
  *
  * Adyen plugin for Shopware 6
  *
- * Copyright (c) 2021 Adyen B.V.
+ * Copyright (c) 2023 Adyen N.V.
  * This file is open source and available under the MIT license.
  * See the LICENSE file for more info.
  *
  */
 
-import './service/adyenService';
-import './component/adyen-config-check-button';
-import './component/adyen-payment-capture';
-import './component/adyen-refund';
-import './component/adyen-notifications';
-import './component/adyen-partial-payments';
-import './sw-order-detail-base-override/index';
-import './sw-order-user-card-override/index';
-import './component/entity/sw-entity-single-select-override';
+const { Component } = Shopware;
+import template from './adyen-partial-payments.html.twig';
 
-import localeEnGb from './snippet/en_GB.json';
+Component.register('adyen-partial-payments', {
+    template,
 
-Shopware.Locale.extend('en-GB', localeEnGb);
+    inject: ['adyenService'],
+
+    // methods: {
+    //     // fetchAdyenPartialPayments() {
+    //     //     this.adyenService.fetchAdyenPartialPayments(this.order.id).then((res) => {
+    //     //         this.partialPayments = res;
+    //     //     });
+    //     // }
+    // },
+
+    beforeMount() {
+       console.log('plugin works fine!');
+    }
+});
