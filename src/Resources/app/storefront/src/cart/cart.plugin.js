0dc7e1f16e430e471b6e61fc4495ab218f40dd28
khushboos <khushboo.singhvi@adyen.com>
2024-01-09 13:28:57 +0100

    Merge branch 'feature/Multigiftcards' of github.com:Adyen/adyen-shopware6 into ECP-8328
    # Conflicts:
    #	src/Controller/StoreApi/OrderApi/OrderApiController.php
    #	src/Service/PaymentStateDataService.php

--- src/Resources/app/storefront/src/cart/cart.plugin.js
+++ src/Resources/app/storefront/src/cart/cart.plugin.js
@@ -228,6 +228,9 @@ export default class CartPlugin extends Plugin {
             //Compare the new total gift card balance with the order amount
             if (this.remainingAmount > 0.00) {
                 //allow adding new giftcards
+                if (this.adyenGiftcardDropDown.length > 0) {
+                    this.adyenGiftcardDropDown[0].style.display = 'block';
+                }
             } else {
                 // Hide giftcards dropdown
                 if (this.adyenGiftcardDropDown.length > 0) {
