826443eaeffb57fb0280521925140757fe10ed6d
sushmita <sushmita.thakur@adyen.com>
2024-05-14 16:34:30 +0200

    hopefully this fixed the path - 4

--- .github/workflows/scripts/prepare-release.sh
+++ .github/workflows/scripts/prepare-release.sh
@@ -6,18 +6,18 @@ cp -r adyen-shopware6 AdyenPaymentShopware6
 #mkdir AdyenPaymentShopware6 && cp -r  AdyenPaymentShopware6
 
 # Remove Shopware dependencies
-composer remove shopware/core --working-dir=AdyenPaymentShopware6
-composer remove shopware/storefront --working-dir=AdyenPaymentShopware6
+composer remove shopware/core --working-dir=./AdyenPaymentShopware6
+composer remove shopware/storefront --working-dir=./AdyenPaymentShopware6
 
 # Remove composer.lock again and vendor directory
 rm AdyenPaymentShopware6/composer.lock
 rm -rf AdyenPaymentShopware6/vendor
 
 # Install dependencies
-composer install --no-dev --working-dir=AdyenPaymentShopware6
+composer install --no-dev --working-dir=./AdyenPaymentShopware6
 
 # Copy original the composer.json file
-cp composer.json AdyenPaymentShopware6/.
+cp adyen-shopware6/composer.json AdyenPaymentShopware6/.
 
 # Zip the clean installation directory
 zip -r AdyenPaymentShopware6.zip AdyenPaymentShopware6/ ; zip -d AdyenPaymentShopware6.zip __MACOSX/\* ; zip -d AdyenPaymentShopware6.zip *.git*
