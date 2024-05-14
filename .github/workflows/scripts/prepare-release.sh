9dcd59f31ba7d2f192fb4476ca33cc5d51527612
sushmita <sushmita.thakur@adyen.com>
2024-05-14 15:31:55 +0200

    Added new workflow file and script for creating zip

---
+++ .github/workflows/scripts/prepare-release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Create a copy of the plugin directory
+cp -r . AdyenPaymentShopware6
+
+# Remove Shopware dependencies
+composer remove shopware/core --working-dir=./AdyenPaymentShopware6
+composer remove shopware/storefront --working-dir=./AdyenPaymentShopware6
+
+# Remove composer.lock again and vendor directory
+rm AdyenPaymentShopware6/composer.lock
+rm -rf AdyenPaymentShopware6/vendor
+
+# Install dependencies
+composer install --no-dev --working-dir=./AdyenPaymentShopware6
+
+# Copy original the composer.json file
+cp ./composer.json AdyenPaymentShopware6/.
+
+# Zip the clean installation directory
+zip -r AdyenPaymentShopware6.zip AdyenPaymentShopware6/ ; zip -d AdyenPaymentShopware6.zip __MACOSX/\* ; zip -d AdyenPaymentShopware6.zip *.git*
