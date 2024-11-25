66b7fc2ee027e0fffa1907cc81bc1947889e172f
Filip Kojic <filip.kojic@logeecom.com>
2024-11-25 10:38:17 +0100

    Add missing comments to different classes
    ISSUE: ADCRSET21I-7

--- src/Service/FetchLogosService.php
+++ src/Service/FetchLogosService.php
@@ -26,15 +26,36 @@ namespace Adyen\Shopware\Service;
 
 use Adyen\Shopware\ScheduledTask\FetchPaymentMethodLogosHandler;
 
+/**
+ * Class FetchLogosService
+ *
+ * A service responsible for providing the handler to fetch payment method logos.
+ * This service acts as an intermediary layer for accessing the FetchPaymentMethodLogosHandler.
+ *
+ * @package Adyen\Shopware\Service
+ */
 class FetchLogosService
 {
+    /**
+     * @var FetchPaymentMethodLogosHandler
+     */
     private FetchPaymentMethodLogosHandler $handler;
 
+    /**
+     * FetchLogosService constructor.
+     *
+     * @param FetchPaymentMethodLogosHandler $handler The handler responsible for fetching payment method logos.
+     */
     public function __construct(FetchPaymentMethodLogosHandler $handler)
     {
         $this->handler = $handler;
     }
 
+    /**
+     * Get the FetchPaymentMethodLogosHandler.
+     *
+     * @return FetchPaymentMethodLogosHandler The handler responsible for executing the logo fetching logic.
+     */
     public function getHandler(): FetchPaymentMethodLogosHandler
     {
         return $this->handler;
