80281f2a719bfac94fe9ac7b520ff6999b84fe14
Filip Kojic <filip.kojic@logeecom.com>
2024-11-28 13:42:27 +0100

    Add Klarna activation on update
    ISSUE: ADCRSET21I-26

--- src/AdyenPaymentShopware6.php
+++ src/AdyenPaymentShopware6.php
@@ -593,9 +593,15 @@ class AdyenPaymentShopware6 extends Plugin
             return;
         }
 
-        if($klarnaDebitRisktMethodId !== null) {
+        if ($klarnaDebitRisktMethodId !== null) {
             // Klarna Debit Risk exists, deactivate Sofort and skip renaming
             $this->deactivateAndRemovePaymentMethod($updateContext, 'Adyen\Shopware\Handlers\SofortPaymentMethodHandler');
+            // activate Klarna Debit Risk
+            $this->setPaymentMethodIsActive(
+                true,
+                $updateContext->getContext(),
+                new PaymentMethods\KlarnaDebitRiskPaymentMethod()
+            );
 
             return;
         }
