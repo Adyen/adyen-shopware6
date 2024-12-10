2c0e193ca0d9e80f3a5b5202890045657ef494b4
Tamara <tamara@example.com>
2024-12-10 10:54:15 +0100

    Add Express checkout configuration
    AD4CR22I-6

--- src/Service/ConfigurationService.php
+++ src/Service/ConfigurationService.php
@@ -272,10 +272,10 @@ class ConfigurationService
     }
 
     /**
-     * @param string $salesChannelId
+     * @param string|null $salesChannelId
      * @return array|mixed|null
      */
-    public function isApplePayExpressCheckoutEnabled(string $salesChannelId)
+    public function isApplePayExpressCheckoutEnabled(string $salesChannelId = null)
     {
         return $this->systemConfigService->get(
             self::BUNDLE_NAME . '.config.applePayExpressCheckoutEnabled',
@@ -284,10 +284,10 @@ class ConfigurationService
     }
 
     /**
-     * @param string $salesChannelId
+     * @param string|null $salesChannelId
      * @return array|mixed|null
      */
-    public function isGooglePayExpressCheckoutEnabled(string $salesChannelId)
+    public function isGooglePayExpressCheckoutEnabled(string $salesChannelId = null)
     {
         return $this->systemConfigService->get(
             self::BUNDLE_NAME . '.config.googlePayExpressCheckoutEnabled',
@@ -296,10 +296,10 @@ class ConfigurationService
     }
 
     /**
-     * @param string $salesChannelId
+     * @param string|null $salesChannelId
      * @return array|mixed|null
      */
-    public function isPayPalExpressCheckoutEnabled(string $salesChannelId)
+    public function isPayPalExpressCheckoutEnabled(string $salesChannelId = null)
     {
         return $this->systemConfigService->get(
             self::BUNDLE_NAME . '.config.payPalExpressCheckoutEnabled',
