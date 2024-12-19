fd159af585d1e6c05def9724b86bdac7f60da2be
Branko Janjic <branko.janjic@logeecom.com>
2024-12-19 09:11:37 +0100

    Add e2e tests to the same repositories

--- .github/workflows/scripts/e2e.sh
+++ .github/workflows/scripts/e2e.sh
@@ -2,7 +2,10 @@
 set -euo pipefail
 
 # Checkout E2E tests
-cd /tmp/adyen-integration-tools-tests;
+cd /tmp;
+git clone https://github.com/Adyen/adyen-integration-tools-tests.git;
+cd adyen-integration-tools-tests;
+git checkout $INTEGRATION_TESTS_BRANCH;
 
 # Setup environment
 rm -rf package-lock.json;
