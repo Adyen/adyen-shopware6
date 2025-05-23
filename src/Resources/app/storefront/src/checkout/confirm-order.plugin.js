63726c496880e2fc269f0db58fd06fd437341930
Filip Kojic <filip.kojic@logeecom.com>
2025-05-23 17:33:03 +0200

    Improve submit button click handling
    ISSUE: CS-6954

--- src/Resources/app/storefront/src/checkout/confirm-order.plugin.js
+++ src/Resources/app/storefront/src/checkout/confirm-order.plugin.js
@@ -640,10 +640,11 @@ export default class ConfirmOrderPlugin extends Plugin {
             paymentMethodInstance.mount(componentSelector);
 
             this.checkoutMainContent.addEventListener('click', function (event) {
-                const confirmFormSubmit = DomAccess.querySelector(document, '#confirmOrderForm button[type="submit"]');
-                if (event.target !== confirmFormSubmit) {
+                const submitButton = event.target.closest('button[type="submit"]');
+                if (!submitButton) {
                     return;
                 }
+                event.preventDefault();
 
                 const form = DomAccess.querySelector(document, '#confirmOrderForm');
                 if (!form.checkValidity()) {
