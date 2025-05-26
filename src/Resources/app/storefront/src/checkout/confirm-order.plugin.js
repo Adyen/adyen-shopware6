cfea3323f12b75807cd749c5c6358de679937832
Filip Kojic <filip.kojic@logeecom.com>
2025-05-26 10:45:04 +0200

    Fix DomAccess fetching
    ISSUE: CS-6938

--- src/Resources/app/storefront/src/checkout/confirm-order.plugin.js
+++ src/Resources/app/storefront/src/checkout/confirm-order.plugin.js
@@ -117,18 +117,12 @@ export default class ConfirmOrderPlugin extends Plugin {
     }
 
     onConfirmOrderSubmit(event) {
-        let confirmFormSubmit;
-        try {
-            confirmFormSubmit = DomAccess.querySelector(document, '#confirmOrderForm button[type="submit"]');
-        } catch (error) {
-            return;
-        }
-
+        const confirmFormSubmit = DomAccess.querySelector(document, '#confirmOrderForm button[type="submit"]', false);
         if (event.target !== confirmFormSubmit) {
             return;
         }
 
-        const form = DomAccess.querySelector(document, '#confirmOrderForm');
+        const form = DomAccess.querySelector(document, '#confirmOrderForm', false);
         if (!form.checkValidity()) {
             return;
         }
@@ -646,12 +640,12 @@ export default class ConfirmOrderPlugin extends Plugin {
             paymentMethodInstance.mount(componentSelector);
 
             this.checkoutMainContent.addEventListener('click', function (event) {
-                const confirmFormSubmit = DomAccess.querySelector(document, '#confirmOrderForm button[type="submit"]');
+                const confirmFormSubmit = DomAccess.querySelector(document, '#confirmOrderForm button[type="submit"]', false);
                 if (event.target !== confirmFormSubmit) {
                     return;
                 }
 
-                const form = DomAccess.querySelector(document, '#confirmOrderForm');
+                const form = DomAccess.querySelector(document, '#confirmOrderForm', false);
                 if (!form.checkValidity()) {
                     return;
                 }
