a3091f952574d186f338be5207830e0dd8a4f4f8
Filip Kojic <filip.kojic@logeecom.com>
2025-09-18 12:47:36 +0200

    Fix express checkout configurations fetching for different sales channels
    ISSUE: CS-7335

--- src/Subscriber/PaymentSubscriber.php
+++ src/Subscriber/PaymentSubscriber.php
@@ -289,9 +289,15 @@ class PaymentSubscriber extends StorefrontSubscriber implements EventSubscriberI
 
         $expressCheckoutConfigurationAvailable = true;
         $expressCheckoutConfiguration = [];
-        $googlePayAvailable = $this->configurationService->isGooglePayExpressCheckoutEnabled();
-        $payPalAvailable = $this->configurationService->isPayPalExpressCheckoutEnabled();
-        $applePayAvailable = $this->configurationService->isApplePayExpressCheckoutEnabled();
+        $googlePayAvailable = $this->configurationService->isGooglePayExpressCheckoutEnabled(
+            $salesChannelContext->getSalesChannelId()
+        );
+        $payPalAvailable = $this->configurationService->isPayPalExpressCheckoutEnabled(
+            $salesChannelContext->getSalesChannelId()
+        );
+        $applePayAvailable = $this->configurationService->isApplePayExpressCheckoutEnabled(
+            $salesChannelContext->getSalesChannelId()
+        );
 
         // If express checkout feature is disabled, returns empty payment method response
         if (!$googlePayAvailable && !$payPalAvailable && !$applePayAvailable) {
@@ -417,9 +423,15 @@ class PaymentSubscriber extends StorefrontSubscriber implements EventSubscriberI
 
         $expressCheckoutConfigurationAvailable = true;
         $expressCheckoutConfiguration = [];
-        $googlePayAvailable = $this->configurationService->isGooglePayExpressCheckoutEnabled();
-        $payPalAvailable = $this->configurationService->isPayPalExpressCheckoutEnabled();
-        $applePayAvailable = $this->configurationService->isApplePayExpressCheckoutEnabled();
+        $googlePayAvailable = $this->configurationService->isGooglePayExpressCheckoutEnabled(
+            $salesChannelContext->getSalesChannelId()
+        );
+        $payPalAvailable = $this->configurationService->isPayPalExpressCheckoutEnabled(
+            $salesChannelContext->getSalesChannelId()
+        );
+        $applePayAvailable = $this->configurationService->isApplePayExpressCheckoutEnabled(
+            $salesChannelContext->getSalesChannelId()
+        );
 
         // If express checkout feature is disabled, returns empty payment method response
         if (!$googlePayAvailable && !$payPalAvailable && !$applePayAvailable) {
