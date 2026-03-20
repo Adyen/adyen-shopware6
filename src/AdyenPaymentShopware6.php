0d3759bf9aa6642605e4ebd00f3683db49854757
Kristina <kristina.mutavdzic@logeecom.com>
2026-03-20 11:11:52 +0100

    Rename Klarna Account to Klarna Pay Over Time
    ISSUE: MAIN-2521

--- src/AdyenPaymentShopware6.php
+++ src/AdyenPaymentShopware6.php
@@ -32,7 +32,6 @@ use Adyen\Shopware\Entity\PaymentResponse\PaymentResponseEntityDefinition;
 use Adyen\Shopware\Entity\PaymentStateData\PaymentStateDataEntityDefinition;
 use Adyen\Shopware\Entity\Refund\RefundEntityDefinition;
 use Adyen\Shopware\Handlers\KlarnaDebitRiskPaymentMethodHandler;
-use Adyen\Shopware\PaymentMethods\KlarnaDebitRiskPaymentMethod;
 use Adyen\Shopware\Service\ConfigurationService;
 use Shopware\Core\Checkout\Payment\PaymentMethodEntity;
 use Shopware\Core\Framework\Plugin;
@@ -53,6 +52,7 @@ use Shopware\Core\System\SalesChannel\SalesChannelEntity;
 class AdyenPaymentShopware6 extends Plugin
 {
     public const SOFORT = 'Adyen\Shopware\Handlers\SofortPaymentMethodHandler';
+    public const KLARNA_ACCOUNT = 'Adyen\Shopware\Handlers\KlarnaAccountPaymentMethodHandler';
 
     /**
      * @param InstallContext $installContext
@@ -173,6 +173,10 @@ class AdyenPaymentShopware6 extends Plugin
 
         if (\version_compare($currentVersion, '5.0.0', '<')) {
             $this->updateTo500($updateContext);
+        }
+
+        if (\version_compare($currentVersion, '5.3.0', '<')) {
+            $this->updateTo530($updateContext);
         }
     }
 
@@ -735,6 +739,33 @@ class AdyenPaymentShopware6 extends Plugin
             ];
             $paymentRepository->update([$paymentData], $updateContext->getContext());
         }
+    }
+
+    /**
+     * @param UpdateContext $updateContext
+     *
+     * @return void
+     */
+    private function updateTo530(UpdateContext $updateContext): void
+    {
+        // Rename Klarna Account to Klarna Pay Over Time
+        $paymentRepository = $this->container->get('payment_method.repository');
+        $paymentMethodId = $this->getPaymentMethodId(self::KLARNA_ACCOUNT);
+
+        if (!$paymentMethodId) {
+            return;
+        }
+
+        $method = new PaymentMethods\KlarnaPayOverTimePaymentMethod();
+
+        $paymentMethodData = [
+            'id' => $paymentMethodId,
+            'handlerIdentifier' => $method->getPaymentHandler(),
+            'name' => $method->getName(),
+            'description' => $method->getDescription(),
+        ];
+
+        $paymentRepository->update([$paymentMethodData], $updateContext->getContext());
     }
 
     /**
