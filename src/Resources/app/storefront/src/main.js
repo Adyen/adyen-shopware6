3ec03e03d5fa6e793bb8946796a84da24266a4ad
Sofija Dokmanovic <sofija.dokmanovic@logeecom.com>
2026-09-23 13:35:04 +0200

    Hide Apple Pay on devices where it is not available
    ISSUE: CS-8514

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -4,11 +4,13 @@ import ConfirmOrderPlugin from './checkout/confirm-order.plugin';
 import AdyenGivingPlugin from './finish/adyen-giving.plugin';
 import AdyenSuccessAction from './finish/adyen-success-action.plugin';
 import ExpressCheckoutPlugin from "./express-checkout/express-checkout.plugin";
+import PaymentMethodAvailabilityPlugin from './checkout/payment-method-availability.plugin';
 
 // Register them via the existing PluginManager
 const PluginManager = window.PluginManager;
 PluginManager.register('CartPlugin', CartPlugin, '#adyen-giftcards-container');
 PluginManager.register('ConfirmOrderPlugin', ConfirmOrderPlugin, '#adyen-payment-checkout-mask');
 PluginManager.register('ExpressCheckoutPlugin', ExpressCheckoutPlugin, '#adyen-express-checkout');
+PluginManager.register('PaymentMethodAvailabilityPlugin', PaymentMethodAvailabilityPlugin, '[data-adyen-availability-check]');
 PluginManager.register('AdyenGivingPlugin', AdyenGivingPlugin, '#adyen-giving-container');
 PluginManager.register('AdyenSuccessAction', AdyenSuccessAction, '#adyen-success-action-container');
